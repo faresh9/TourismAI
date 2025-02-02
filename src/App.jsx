27ea5db969827569b4dbc9ba0548a143bd039447
fares haroun <far0213025@ju.edu.jo>
2025-02-02 21:38:04 +0300

    chore: Remove unused components and simplify App structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import { useState } from 'react';
-import { Header, ContentInput, GeneratedContent } from './components';
-import './styles/main.css';
-import { generateContentWithOpenAI } from './openai'; // Import OpenAI function
+import React from 'react'
 
 function App() {
-  const [generatedContent, setGeneratedContent] = useState('');
-
-  const generateContent = async (destination, selectedPlace) => {
-    try {
-      const openaiApiKey = import.meta.env.VITE_REACT_APP_OPENAI_API_KEY;
-
-      const generatedContent = await generateContentWithOpenAI(
-        destination,
-        selectedPlace,
-        openaiApiKey
-      );
-      console.log('key');
-      setGeneratedContent(generatedContent);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
   return (
-    <div className="App">
-      <Header />
-      <ContentInput onGenerateContent={generateContent} />
-      <GeneratedContent content={generatedContent} />
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- src/components/index.jsx
+++
@@ -1,5 +0,0 @@
-//src/components/index.jsx
-export { default as GeneratedContent } from './GeneratedContent'
-export { default as ContentInput } from './ContentInput'
-export { default as Header } from './Header'
-
