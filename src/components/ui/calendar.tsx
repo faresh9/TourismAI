c7201a2a0c5e48bcf19b64d1551074881f6fe626
fares haroun <far0213025@ju.edu.jo>
2025-02-14 14:54:21 +0300

    refactor: Update Calendar component to set start and end month for improved date range control

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -52,10 +52,8 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
-      }}
+      startMonth={new Date(2025, 0)}
+      endMonth={new Date(2028, 11)}
       {...props}
     />
   );
